6b25797d98ef82e7c10d27dba99cd63595ee88ed
MinkoV <minkovforjob@gmail.com>
2026-09-11 09:27:27 +0200

    interface 3 step

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
 // * Удаление заметок, * Поиск, * Редактирование заметки
+const pencilBtn = document.querySelector("#pencilBtn");
+const formAddTask = document.querySelector(".formAddTask");
+const addTaskBtn = document.querySelector("#addTaskBtn");
+const cancelBtn = document.querySelector("#cancelBtn");
+const textTask = document.querySelector("textTask");
+const dateTimeTask = document.querySelector("dateTimeTask");
+
+
+
+// кнопка "Карандаш"
+pencilBtn.addEventListener("click", () => {
+
+    formAddTask.setAttribute("style", "display: flex;");
+
+})
+
+cancelBtn.addEventListener("click", () => { formAddTask.setAttribute("style", "display: none;"); })
+
+addTaskBtn.addEventListener("click", () => {
+    console.log("textTask");
+    console.log("dateTimeTask");
+})
