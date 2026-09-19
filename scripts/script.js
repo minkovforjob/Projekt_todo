6f79c32e429e3d2101b3ebb149b79cdbc02ec231
MinkoV <minkovforjob@gmail.com>
2026-09-19 23:33:18 +0200

    process1

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
 // * Удаление заметок, * Поиск, * Редактирование заметки
+
 const pencilBtn = document.querySelector("#pencilBtn");
+const findBtn = document.querySelector("#findBtn");
+const findInput = document.querySelector("#findInput");
+
 const formAddTask = document.querySelector(".formAddTask");
 const addTaskBtn = document.querySelector("#addTaskBtn");
 const cancelBtn = document.querySelector("#cancelBtn");
+
 const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 const taskList = document.querySelector("#taskList");
+console.log(tasks);
 const tabAll = document.querySelector("#tabAll");
 const tabActive = document.querySelector("#tabActive");
 const tabEnded = document.querySelector("#tabEnded");
@@ -12,6 +18,31 @@ const spanAll = document.querySelector("#spanAll");
 const spanActive = document.querySelector("#spanActive");
 const spanEnded = document.querySelector("#spanEnded");
 
+//      Текущая дата
+const currentDateMonthYear = document.querySelector("#currentDateMonthYear");
+const currentWeekDay = document.querySelector("#currentWeekDay");
+const now = new Date();
+// console.log(now.getFullYear()); // год
+// console.log(now.getMonth());    // месяц (0–11!)
+// console.log(now.getDate());     // день месяца
+const monthName = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июль", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+
+currentDateMonthYear.textContent = `${now.getDate()} ${monthName[now.getMonth()]} ${now.getFullYear()}`
+
+const days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота"
+];
+currentWeekDay.textContent = days[now.getDay()];
+
+
+
+updateVisualTaskList(tasks);
 // console.log(taskList);
 
 
@@ -22,35 +53,21 @@ const spanEnded = document.querySelector("#spanEnded");
 //     checked: false
 // };
 
-// document.addEventListener("click", (event) => {
-//     if (!formAddTask.contains(event.target)) {
-//         console.log("formAddTask");
-//         formAddTask.classList.add("hidden");
-//     }
-//     // if (pencilBtn.contains(event.target)) {
-//     //     console.log("pencilBtn");
-//     //     // formAddTask.classList.remove("hidden");
-//     // }
+tasks.indexOf
+function delTask(idTask) {
+    tasks.for
+    // tasks.splice(i, 1);
 
-//     // console.log(event);
-// });
 
-function updateVisualTaskList() {
+}
 
-    for (let task of tasks) {
+function updateVisualTaskList(listOfTasks) {
+    taskList.replaceChildren();
+    for (let task of listOfTasks) {
 
         const el = document.createElement("li");
         el.setAttribute("class", "oneTask");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = task.checked;
-        checkbox.addEventListener("change", (event) => {
-            // console.log("Checkbox изменился");
-            // console.log(event.target.checked);
-            task.checked = event.target.checked;
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-
-        });
+        el.dataset.id = task.id;
         const taskInfo = document.createElement("div");
         taskInfo.setAttribute("class", "taskInfo");
         const taskText = document.createElement("span");
@@ -59,26 +76,67 @@ function updateVisualTaskList() {
         const taskDate = document.createElement("span");
         taskDate.setAttribute("class", "taskDate");
         taskDate.textContent = task.dateTimeTask;
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = task.checked;
+        if (task.checked) { taskText.classList.add("checked") }
+        else { taskText.classList.remove("checked") }
+        checkbox.addEventListener("change", (event) => {
+            // console.log("Checkbox изменился");
+            // console.log(event.target.checked);
+            task.checked = event.target.checked;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            if (task.checked) { taskText.classList.add("checked") }
+            else { taskText.classList.remove("checked") }
+        });
+
+        const delTaskBtn = document.createElement("button")
+        delTaskBtn.setAttribute("class", "delTask");
+        delTaskBtn.textContent = "x";
+        delTaskBtn.addEventListener("click", (event) => {
+            // const idForDel = event.target.id;
+            // console.log(event.target.closest(".oneTask"));
+            // console.log(event.target.closest(".oneTask").dataset.id);
+
+            delTask(event.target.closest(".oneTask").dataset.id);
+        })
+
+
+
+
         taskInfo.appendChild(taskDate);
         taskInfo.appendChild(taskText);
         el.appendChild(checkbox);
         el.appendChild(taskInfo);
+        el.appendChild(delTaskBtn);
         // el.appendChild(checkbox);
         taskList.appendChild(el);
     }
 }
 
-updateVisualTaskList();
+//************  sort */
+function sortTasks() {
+    tasks.sort((a, b) => { return new Date(a.dateTimeTask) - new Date(b.dateTimeTask) });
+}
 
 
 //  **************************      Filter      *******************
-tabAll.addEventListener("click", () => {
+
+function allTasksSettings() {
     tabAll.classList.add("filtred");
     tabActive.classList.remove("filtred");
     tabEnded.classList.remove("filtred");
     spanAll.textContent = "✓";
     spanActive.textContent = "";
     spanEnded.textContent = "";
+
+
+}
+
+tabAll.addEventListener("click", () => {
+    allTasksSettings();
+    updateVisualTaskList(tasks);
 })
 
 tabActive.addEventListener("click", () => {
@@ -88,6 +146,10 @@ tabActive.addEventListener("click", () => {
     spanAll.textContent = "";
     spanActive.textContent = "✓";
     spanEnded.textContent = "";
+
+    const tasksActive = tasks.filter((task) => { return (!task.checked); })
+    console.log(tasksActive);
+    updateVisualTaskList(tasksActive);
 })
 
 tabEnded.addEventListener("click", () => {
@@ -97,28 +159,58 @@ tabEnded.addEventListener("click", () => {
     spanAll.textContent = "";
     spanActive.textContent = "";
     spanEnded.textContent = "✓";
+    const tasksEnded = tasks.filter((task) => { return (task.checked); })
+    console.log(tasksEnded);
+    updateVisualTaskList(tasksEnded);
 })
 
+// find
+findBtn.addEventListener("click", () => {
+    if (findInput.value !== "") {
+        allTasksSettings();
+        const findTasks = tasks.filter((task) => { return (task.textTask.toLowerCase().includes(findInput.value.toLowerCase())) })
+        console.log(findTasks);
+        updateVisualTaskList(findTasks);
+    }
+    else {
+        allTasksSettings();
+        updateVisualTaskList(tasks);
+    }
 
+})
 
 // кнопка "Карандаш"
-pencilBtn.addEventListener("click", () => {
-
+pencilBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
     formAddTask.classList.remove("hidden");
-
+    textStatusMessage.textContent = "";
 });
 
+
+// ****   close  formAddTask
 formAddTask.addEventListener("reset", (event) => {
     formAddTask.classList.add("hidden");
 });
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") { formAddTask.classList.add("hidden"); }
+});
+document.addEventListener("click", () => {
+    formAddTask.classList.add("hidden");
+});
+
+
+// ****     formAddTask
+formAddTask.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
 
 formAddTask.addEventListener("submit", (event) => {
-
-
+    event.stopPropagation();
     event.preventDefault();
     let inp_textTask = event.target.elements.textTask;
     let inp_dateTimeTask = event.target.elements.dateTimeTask;
     if ((inp_textTask.value) && (inp_dateTimeTask.value)) {
+        textStatusMessage.textContent = "";
         const random = Math.floor(Math.random() * 100);
         console.log(inp_textTask);
         console.log(inp_dateTimeTask);
@@ -134,22 +226,16 @@ formAddTask.addEventListener("submit", (event) => {
         localStorage.setItem("tasks", JSON.stringify(tasks));
 
 
-
-
-
-
-
-
-
-
+        // подумать куда лучше поставить сортировку
+        sortTasks();
 
         inp_textTask.value = "";
         inp_dateTimeTask.value = "";
     } else {
-        // if (textStatusMessage.classList.contains("OKMassage")) {
-        //     textStatusMessage.classList.replace("OKMassage", "errorMassage");
-        // }
-        // textStatusMessage.textContent = "Все поля должны быть заполнены!";
+        if (textStatusMessage.classList.contains("OKMassage")) {
+            textStatusMessage.classList.replace("OKMassage", "errorMassage");
+        }
+        textStatusMessage.textContent = "Все поля должны быть заполнены!";
     }
     // console.log(textTask);
     // console.log(dateTimeTask);
@@ -158,10 +244,16 @@ formAddTask.addEventListener("submit", (event) => {
 
 });
 
+// const oneTask = document.querySelector(".oneTask");
+// //  ************          edit task
+// oneTask.addEventListener("dblclick", (event) => {
+//     formAddTask.classList.remove("hidden");
+//     textStatusMessage.textContent = "";
 
-// cancelBtn.addEventListener("click", () => { formAddTask.setAttribute("style", "display: none;"); })
+//     // let inp_textTask.value = event.target.taskText.value;
+//     // let inp_dateTimeTask.value = event.target.dateTimeTask.value;
+//     console.log(event.target);
+//     console.log(event.target.dateTimeTask);
 
-// addTaskBtn.addEventListener("click", () => {
-//     console.log("textTask");
-//     console.log("dateTimeTask");
 // })
+
